56b85b3b578dae7ad5ff43fea157766b8dedc559
live4code <steven.marelly@gmail.com>
2020-06-29 12:41:41 +0300

    Date editor

--- php/includes/add.php
+++ php/includes/add.php
@@ -1,2 +1,21 @@
 <?php
+  session_start();
+  require_once "connect.php";
+
+  $author = $_SESSION["user"]["name"];
+
+  $author_id = mysqli_fetch_assoc(mysqli_query($connect, "SELECT * FROM `authors` WHERE name = '$author'"))["id"];
+
+  $type = $_POST["type"];
+  $title = $_POST["title"];
+  $description = $_POST["description"];
+
+  $path = time().$_FILES['img']["name"];
+  move_uploaded_file($_FILES["img"]["tmp_name"], "../files/".$path);
+  $date = date("Y-m-d");
+
+  mysqli_query($connect, "INSERT INTO `articles` (`id`, `theme_id`, `author_id`, `title`, `description`, `date`, `img`) VALUES (NULL, '$type', '$author_id', '$title', '$description', '$date', '$path')");
+  
+  header('Location: /index.html');
+
 ?>
--- php/urls/authors.php
+++ php/urls/authors.php
@@ -4,15 +4,22 @@
   }
   else{
     if($_GET["who"] == "add"){
-      echo '<h2 class="main__title">ПРЕДЛОЖИТЬ НОВОСТЬ</h2>
-              <form class="col-md-8 text-body font-weight-bold row" action = "php/includes/add.php" method = "post">
+      echo '<h4 class="main__title">ПРЕДЛОЖИТЬ НОВОСТЬ</h4>
+              <form class="col-md-8 text-body font-weight-bold row" action = "php/includes/add.php" method = "post" enctype="multipart/form-data">
                 <div class="form-group">
                   <label class="mb-0 mt-3" for="select-theme">ВЫБЕРЕТЕ ТЕМУ:</label>
                   <select class="custom-select" id="select-theme" name = "type" required>
                     <option selected disabled>ВЫБЕРИТЕ ТЕМУ:</option>
                     <option value="1">СПОРТ</option>
                     <option value="2">МУЗЫКА</option>
-                    <option value="3">ПОЛИТИКА</option>
+                    <option value="3">КИНО</option>
+                    <option value="4">ПУТЕШЕСТВИЯ</option>
+                    <option value="5">БИЗНЕС</option>
+                    <option value="6">ИСТОРИЯ</option>
+                    <option value="7">ИСКУССТВО</option>
+                    <option value="8">МОДА</option>
+                    <option value="9">ПОЛИТИКА</option>
+                    <option value="10">ТЕХНОЛОГИИ</option>
                   </select>
                   <label class="mb-0 mt-3" for="name-article">НАЗВАНИЕ СТАТЬИ (максимум 60 символов):</label>
                   <input type="text" class="form-control" id="name-article" placeholder="ВВЕДИТЕ НАЗВАНИЕ СТАТЬИ" value=""
